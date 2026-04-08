e846c096e2229d5fd3eb647fd5081c7d56cbe06f
Luma Frazão <105181464+lumafrazao-dev@users.noreply.github.com>
2026-04-08 06:31:15 -0300

    rvviews.py

--- agendamento/views.py
+++ agendamento/views.py
@@ -1,38 +1,30 @@
 from django.shortcuts import render, redirect
 from datetime import datetime, timedelta
-from .models import Agendamento
-
+from .models import Agendamento #cod base
 
 def gerar_horarios(data):
-    horarios = []
-
-    # horário de funcionamento
-    if data.weekday() == 5:  # sábado
+    horarios = [] #guarda os hrs
+    if data.weekday() == 5:  #sab
         inicio = datetime.combine(data, datetime.min.time()).replace(hour=9, minute=0)
         fim = datetime.combine(data, datetime.min.time()).replace(hour=14, minute=0)
-    else:  # segunda a sexta
+    else:  #swg a ses
         inicio = datetime.combine(data, datetime.min.time()).replace(hour=9, minute=0)
         fim = datetime.combine(data, datetime.min.time()).replace(hour=17, minute=30)
 
-    # gerar horários de 30 em 30 min
-    atual = inicio
+    atual = inicio #gera intervalo 30min
     while atual <= fim:
         horarios.append(atual.strftime("%H:%M"))
         atual += timedelta(minutes=30)
 
-    # 🔥 buscar horários já ocupados no banco
+    #busca is hrs ocupados no banco
     ocupados = Agendamento.objects.filter(data=data).values_list('horario', flat=True)
-
-    # remover horários ocupados
+    # exclui os hrs ocupados
     horarios_disponiveis = [h for h in horarios if h not in ocupados]
-
     return horarios_disponiveis
 
-
-def home(request):
+def home(request): #formulario
     horarios = []
     dados = {}
-
     if request.method == "POST":
         nome = request.POST.get("nome")
         telefone = request.POST.get("telefone")
@@ -40,22 +32,18 @@ def home(request):
         data_str = request.POST.get("data")
         horario = request.POST.get("horario")
         print("HORARIO RECEBIDO:", horario)
-        
-
+     
         dados = {
             "nome": nome,
             "telefone": telefone,
             "placa": placa,
             "data": data_str
         }
-
         if data_str:
             data = datetime.strptime(data_str, "%Y-%m-%d").date()
-
-            # 🔥 SE VEIO HORÁRIO → SALVA NO BANCO
+            #se preenchido salva no banco
             if horario:
                 existe = Agendamento.objects.filter(data=data, horario=horario).exists()
-
                 if not existe:
                     Agendamento.objects.create(
                         nome=nome,
@@ -64,14 +52,11 @@ def home(request):
                         data=data,
                         horario=horario
                     )
-
-                # 🔥 REDIRECT (ESSA LINHA RESOLVE TUDO)
                 return redirect("/")
-
-            # 🔥 GERAR HORÁRIOS DISPONÍVEIS
-            if data.weekday() != 6:  # bloqueia domingo
+            #gera os hrs disponiveis
+            if data.weekday() != 6:  #block os dom
                 horarios = gerar_horarios(data)
-
+                
     return render(request, "agendamento.html", {
         "horarios": horarios,
         "dados": dados
